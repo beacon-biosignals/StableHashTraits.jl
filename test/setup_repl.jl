c52ffdf4457586221aa1dc311d529418e1aa54dc
David Little <david.frank.little@gmail.com>
2023-09-05 13:42:28 -0400

    test setup

---
+++ test/setup_repl.jl
@@ -0,0 +1,8 @@
+include("setup_tests.jl")
+
+crc(x, s=0x000000) = crc32c(collect(x), s)
+V = 1
+hashfn = sha256 # sha1, crc
+ctx = HashVersion{V}()
+test_hash(x, c=ctx) = stable_hash(x, c; alg=hashfn)
+
