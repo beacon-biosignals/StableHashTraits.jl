020603062d63e9bb10fb76f4314244df78ff49c6
David Little <david.frank.little@gmail.com>
2024-01-10 11:24:56 -0500

    formatting

--- test/ValidateGenerated/test/runtests.jl
+++ test/ValidateGenerated/test/runtests.jl
@@ -1,6 +1,7 @@
 # NOTE: this should be run with julia --compiled-modules=no
 using Test
 @testset "StableHashTraits `@generated` functions are properly defined" begin
-    import StableHashTraits, CRC32c
+    using StableHashTraits: StableHashTraits
+    using CRC32c: CRC32c
     @test StableHashTraits.stable_hash("hello"; alg=CRC32c.crc32c, version=2) == 0x9f8db41f
 end
