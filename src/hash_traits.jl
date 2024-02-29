6986e744230fbec38c588ad97565baf66b9c4a3f
David Little <david.frank.little@gmail.com>
2024-02-29 09:23:57 -0500

    wip

--- src/hash_traits.jl
+++ src/hash_traits.jl
@@ -105,9 +105,6 @@ end
 
 function stable_hash_helper(x, hash_state, context, st::StructTypes.DataType)
     nested_hash_state = start_nested_hash!(hash_state)
-    nested_hash_state = stable_hash_helper(@hash64("DataType"),
-                                           nested_hash_state, context,
-                                           StructTypes.NumberType())
 
     # hash the field values themselves
     fields = st isa StructTypes.UnorderedStruct ? sorted_field_names(typeof(x)) :
@@ -116,16 +113,20 @@ function stable_hash_helper(x, hash_state, context, st::StructTypes.DataType)
         val = getfield(x, field)
         # field types that are concrete have already been accounted for in the type hash of
         # `x` so we can skip them
-        # TODO: I have to think carefully about this
-        # it isn't quite all adding up because `st` in this context makes no sense
-        # (so what do you pick)
         if !is_concrete_type(x, field)
-            # YES: we do hash the type *before* transformation; we can think of the
-            # transformation as mutating the hash content; the reason for hashing types is
-            # to ensure that content that so happens to be byte equivalent doesn't hash to
-            # the same value if its a different type, so we don't have to account for the
-            # type post transformation, and doing so avoids a lot complexity
-            stable_type_hash(val, hash_state, context, st)
+            # YES: we do hash the type *before* transformation; if `transform` is type
+            # stable we can generally think of the type hash before or after transform as
+            # equivalent. If it isn't, the type before hashing may still be disambguiate the
+            # hashed transform content so long as all unique byte sequences hashed hashed
+            # due to the `transform` uniquely map to a single type. In practice, for sane
+            # uses of transform, this will be the case. If we were to hash the type *after*
+            # transformation we would then need to *require* `transform` to be type stable,
+            # since otherwise the type hash would vary depending on the value passed to
+            # `transform`. Thus, by doing this operation before means we the assumptions
+            # placed on `transform` are weaker. To avoid these assumptions entirely, we'd
+            # have to always hash the type for every value, which reduces performance by
+            # ~10-100x fold in the benchmarks.
+            stable_type_hash(val, hash_state, context, HashType(val, st))
         end
 
         tval = transform(val, context)
@@ -147,9 +148,6 @@ sort_items_by(x) = nothing
 function stable_hash_helper(x, hash_state, context, ::StructTypes.DictType)
     pairs = StructTypes.keyvaluepairs(x)
     nested_hash_state = start_nested_hash!(hash_state)
-    nested_hash_state = stable_hash_helper(@hash64("DictType"),
-                                           nested_hash_state, context,
-                                           StructTypes.NumberType())
 
     pairs = isnothing(sorted_items_by(x)) ? StructTypes.keyvaluepairs(x) :
             sort(StructTypes.keyvaluepairs(x); by=sort_items_by)
@@ -179,7 +177,6 @@ end
 transform(x::AbstractVector, c::HashVersion{3}) = x
 HashType(x::AbstractRange, c::HashVersion{3}) = StructTypes.Struct()
 
-# TODO: don't forget about tuples
 function has_concrete_eltype(xs)
     return isdispatchtuple(typeof(xs))
 end
@@ -197,21 +194,27 @@ function stable_type_hash(T::Type, hash_state, context, ::StructTypes.ArrayType)
     return update_hash!(hash_state, bytes, context)
 end
 
-function stable_hash_helper(xs, hash_state, context, st::StructTypes.ArrayType)
+function stable_hash_helper(xs, hash_state, context, ::StructTypes.ArrayType)
     nested_hash_state = start_nested_hash!(hash_state)
 
-    tag = xs isa Tuple ? @hash64("Base.Tuple") : @hash64("ArrayType")
     nested_hash_state = stable_hash_helper(tag, nested_hash_state, context,
                                            StructTypes.NumberType())
 
     items = order_matters(x) ? sort(xs; by=string) : xs
-    if has_concrete_eltype(xs)
-    else
+    if has_concrete_eltype(items)
+        x1 = first(items)
+        stable_type_hash(x1, nested_hash_state, context, HashType(x1, content))
         for x in items
-            stable_type_hash(x, nested_hash_state, context, )
             tx = transform(x, context)
             nested_hash_state = stable_hash_helper(tx, nested_hash_state, context,
-                                                HashType(tx, context))
+                                                   HashType(tx, context))
+        end
+    else
+        for x in items
+            stable_type_hash(x, nested_hash_state, context, HashType(x, context))
+            tx = transform(x, context)
+            nested_hash_state = stable_hash_helper(tx, nested_hash_state, context,
+                                                   HashType(tx, context))
         end
     end
 
@@ -220,8 +223,54 @@ function stable_hash_helper(xs, hash_state, context, st::StructTypes.ArrayType)
 end
 
 #####
+##### Tuples
+#####
+
+function stable_type_hash(T::Type{<:Tuple}, hash_state, context, ::StructTypes.ArrayType)
+    bytes = get!(context, T) do
+        type_context = HashVersion{3}()
+        type_hash_state = similar_hash_state(hash_state)
+        type_hash_state = stable_hash_helper(qualified_name_(T), type_hash_state, type_context,
+                           StructTypes.StringType())
+        for f in fieldnames(T)
+            type_hash_state = stable_type_hash(fieldtype(T, f), type_hash_state,
+                                                type_context, StructType(T))
+        end
+        return compute_hash!(type_hash_state)
+    end
+    return update_hash!(hash_state, bytes, context)
+end
+
+function stable_hash_helper(x::Tuple{<:Tuple}, hash_state, context, st::StructTypes.ArrayType)
+    nested_hash_state = start_nested_hash!(hash_state)
+
+    # hash the tuple field values themselves
+    for field in fieldnames(typeof(x))
+        val = getfield(x, field)
+        # field types that are concrete have already been accounted for in the type hash of
+        # `x` so we can skip them
+        if !is_concrete_type(x, field)
+            # TODO: reference note about when we hash types
+            stable_type_hash(val, hash_state, context, HashType(val, st))
+        end
+
+        tval = transform(val, context)
+        nested_hash_state = stable_hash_helper(tval, nested_hash_state, context,
+                                               HashType(tval, context))
+    end
+
+    hash_state = end_nested_hash!(hash_state, nested_hash_state)
+    return hash_state
+end
+
+
+#####
 ##### CustomStruct
 #####
+
+function stable_type_hash(x, hash_state, context, ::StructTypes.CustomStruct)
+    # well shoot... this doesn't work
+end
 
 function stable_hash_helper(x, hash_state, context, ::StructTypes.CustomStruct)
     lowered = StructTypes.lower(x)
