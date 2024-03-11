c8f8f197f23707a32e8ccadff4141f2a7a30008a
David Little <david.frank.little@gmail.com>
2024-03-11 17:01:06 -0400

    debugging doc CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,6 @@
 using StableHashTraits
 using Documenter
 
-DocMeta.setdocmeta!(StableHashTraits, :DocTestSetup, :(using StableHashTraits))
-
 readme_file = joinpath(pkgdir(StableHashTraits), "README.md")
 readme = read(readme_file, String)
 overview_txt = match(r"START_OVERVIEW-->(.*?)<!--END_OVERVIEW"s, readme).captures[1]
@@ -14,12 +12,16 @@ index_str = replace(index_str, "{INSERT_EXAMPLE}" => example_txt)
 
 link_pattern = r"\[`(\S+)`\]\((https://beacon-biosignals\.github\.io/StableHashTraits\.jl/stable/\S+)\)"
 index_str = replace(index_str, link_pattern => s"[`\1`](@ref)")
-write(joinpath(@__DIR__, "src/index.md"), index_str)
+write(joinpath(@__DIR__, "src", "index.md"), index_str)
 
 pages = ["Manual" => "index.md",
          "API" => "api.md",
          "Deprecated" => "deprecated.md"]
 
+source_files = readdir(joinpath(@__DIR__, "src"))
+@info "Documentation source: " source_files
+
+DocMeta.setdocmeta!(StableHashTraits, :DocTestSetup, :(using StableHashTraits))
 makedocs(; modules=[StableHashTraits], sitename="StableHashTraits.jl",
          authors="Beacon Biosignals", pages)
 rm("src/index.md")
