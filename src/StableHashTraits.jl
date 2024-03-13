30ff0cd5b99dea08ec0266f5a3e996500b35e3d8
David Little <david.frank.little@gmail.com>
2024-03-13 10:35:28 -0400

    fix generated-function order issue

--- src/StableHashTraits.jl
+++ src/StableHashTraits.jl
@@ -17,17 +17,23 @@ export stable_hash, HashVersion, stable_type_name
 include("hash_algorithms.jl")
 # update_hash!, HashState, compute_hash!, start_nested_hash!, end_nested_hash!, similar_hash_state
 
-include("deprecated.jl")
-export WriteHash, IterateHash, StructHash, FnHash, ConstantHash, @ConstantHash,
-       HashAndContext, HashVersion, qualified_name, qualified_type, TablesEq, ViewsEq,
-       stable_typename_id, stable_type_id
-
 include("caching_context.jl")
 export CachedHash
 # HashShouldCache
 
+# deprecated type used by `hash_method` that needs to be defined earlier than the other
+# `deprecated.jl` content (to be used for a deprecation check in `hash_traits.jl`)
+struct NotImplemented end
+is_implemented(::NotImplemented) = false
+is_implemented(_) = true
+
 include("hash_traits.jl")
 # stable_hash_helper, type_hash_name, type_value_name, type_structure
+
+include("deprecated.jl")
+export WriteHash, IterateHash, StructHash, FnHash, ConstantHash, @ConstantHash,
+       HashAndContext, HashVersion, qualified_name, qualified_type, TablesEq, ViewsEq,
+       stable_typename_id, stable_type_id
 
 include("special_contexts.jl")
 export TablesEq, ViewsEq, WithTypeNames
