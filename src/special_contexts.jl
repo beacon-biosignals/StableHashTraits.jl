b8c54f59fe12da1e6f551c10e0bc5a5de15316ec
David Little <david.frank.little@gmail.com>
2024-04-14 21:57:25 -0400

    fix type in previous cleanup

--- src/special_contexts.jl
+++ src/special_contexts.jl
@@ -93,12 +93,12 @@ parent_context(x::ViewsEq) = x.parent
 # `HashVersion{1}` as a parent or ancestor, but make use of the updated, more optimized API
 # for `HashVersion{2}`
 function hash_method(::AbstractArray, c::ViewsEq)
-    root_version(m) > 2 && return NotImplemented()
+    root_version(c) > 2 && return NotImplemented()
     return (root_version(c) > 1 ? @ConstantHash("Base.AbstractArray") :
             PrivateConstantHash("Base.AbstractArray"), FnHash(size), IterateHash())
 end
 function hash_method(::AbstractString, c::ViewsEq)
-    root_version(m) > 2 && return NotImplemented()
+    root_version(c) > 2 && return NotImplemented()
     return (root_version(c) > 1 ? @ConstantHash("Base.AbstractString") :
             PrivateConstantHash("Base.AbstractString", WriteHash()), WriteHash())
 end
