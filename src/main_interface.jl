323a6f94fec6063180ffd697cc327fea4e3f1809
David Little <david.frank.little@gmail.com>
2024-06-07 12:03:37 -0400

    merge cleanup

--- src/main_interface.jl
+++ src/main_interface.jl
@@ -1,7 +1,7 @@
 """
     HashVersion{V}()
 
-The default `hash_context` used by `stable_hash`. There are currently three versions (1-4).
+The default `hash_context` used by `stable_hash`. There are currently four versions (1-4).
 Version 4 should be favored when at all possible. Version 1 is the default version, so as to
 avoid changing the hash computed by existing code.
 
