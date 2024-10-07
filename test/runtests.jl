4fe0987b2d506ef8ef2f1ead68e379fa81f4a571
David Little <david.frank.little@gmail.com>
2024-10-07 17:03:48 -0400

    formatting

--- test/runtests.jl
+++ test/runtests.jl
@@ -254,10 +254,11 @@ include("setup_tests.jl")
                     @test test_hash(Array{Int,3}) != test_hash(Array{Int,4})
                     @test test_hash(Array{<:Any,3}) != test_hash(Array{<:Any,4})
 
-                    # NOTE: these should run without a `StackOverflowError` (previously it
+                    # NOTE: these should run without an `StackOverflowError` (previously it
                     # did overflow)
-                    @test test_hash((;a=Vector{Int})) != test_hash((;a=Vector{String}))
-                    @test test_hash((;a=Array{T,1} where {T})) !== test_hash((;a=Array{T,2} where {T}))
+                    @test test_hash((; a=Vector{Int})) != test_hash((; a=Vector{String}))
+                    @test test_hash((; a=Array{T,1} where {T})) !=
+                          test_hash((; a=Array{T,2} where {T}))
                 end
             end
 
